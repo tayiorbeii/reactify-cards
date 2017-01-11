4904284d94247c13e15c70a861063cdd0ac2c2cd
Taylor Bell <taylorbell@gmail.com>
2017-01-11 13:50:02 -0700

    fix import order

--- reactify-card/src/index.js
+++ reactify-card/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StaticCourseCard, StaticLessonCard, StaticPlaylistCard } from './StaticCards'
-import 'tachyons-egghead'
 import 'font-awesome/css/font-awesome.css'
+import 'tachyons-egghead'
+import { StaticCourseCard, StaticLessonCard, StaticPlaylistCard } from './StaticCards'
 
 ReactDOM.render(
   <div className='flex flex-column justify-center items-center content-around bg-dark-navy pv5'>
