49310febd2c74d74e7e6248f50cf5bda517bc3d9
Taylor Bell <taylorbell@gmail.com>
2017-01-13 14:37:27 -0700

    progress

--- reactify-card/src/Card.js
+++ reactify-card/src/Card.js
@@ -6,13 +6,17 @@ const courseCardClasses = `${commonCardClasses} card-stacked-shadow card-course`
 const lessonCardClasses = `${commonCardClasses} card-lesson`
 const commonInnerClasses = 'flex flex-column items-center br2 bg-white navy relative z-1 card-course-inner'
 
+const titleHeadingClasses = 'f3 tc mt4 mb2 avenir fw5'
+const authorNameClasses = 'f6 dark-gray o-50 mb4 tc'
+
 export const Card = () => {
   return (
     <div className={commonCardClasses}>
       <div className={commonInnerClasses}>
+        <h3 className={titleHeadingClasses}>Introduction to RxJS Marble Testing Two lines headline</h3>
+        <div className={authorNameClasses}>Joe Maddalone</div>
       </div>
     </div>
   )
 }
-
 
